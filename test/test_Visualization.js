e88ce6b2a6d55a8321298a404ed56519b7bed758
Julia Baum <j.baum@stud.uni-heidelberg.de>
2020-11-28 14:23:14 +0100

    CONDEC-836: add test for CONDEC-492

--- test/test_Visualization.js
+++ test/test_Visualization.js
@@ -14,13 +14,7 @@ describe('TCS: CONDEC-177', () => {
   });
   it('should generate the correct treant vis graph', async () => {
     try {
-      // const issueTypes = await getIssueTypes();
-      // const issueTypeNames = [];
-      // issueTypes.forEach((issueTypeObject) => {
-      //   issueTypeNames.push(issueTypeObject.name);
-      // });
-
-      const createdIssue = await createJiraIssue('Task', 'Dummy task');
+      const createdIssue = await createJiraIssue('Task', 'Dummy treant vis graph task');
 
       const commentString = `{issue}How should we brew coffee?{issue}
       {decision}Use a french press to brew coffee!{decision}
@@ -36,7 +30,6 @@ describe('TCS: CONDEC-177', () => {
         .post(`${JSONConfig.fullUrl}/rest/condec/latest/view/getTreant.json`,
           searchPayload,
           localCredentialsObject);
-
       // check that the tree has contains the correct data by traversing the graph
 
       const rootLevel = graph.data.nodeStructure.children;
@@ -64,9 +57,52 @@ describe('TCS: CONDEC-177', () => {
     }
   });
 });
-describe('TCS: CONDEC-177', () => {
+
+describe('TCS: CONDEC-492', () => {
   before(async () => {
     await setUpJira();
   });
-  it('should generate ')
+  it('should generate the correct vis.js graph', async () => {
+    try {
+      const createdIssue = await createJiraIssue('Task', 'Dummy vis.js task');
+
+      const commentString = `{issue}How should we brew coffee?{issue}
+      {decision}Use a french press to brew coffee!{decision}
+      {alternative}Use a filter coffee machine{alternative}`;
+
+      await jira.addComment(createdIssue.key, commentString);
+      const searchPayload = {
+        searchTerm: '',
+        selectedElement: createdIssue.key,
+        projectKey: JSONConfig.projectKey,
+      };
+      const graph = await axios
+        .post(`${JSONConfig.fullUrl}/rest/condec/latest/view/getVis.json`,
+          searchPayload,
+          localCredentialsObject);
+
+      // sort here so we can guarantee the order for later comparisons
+      const visNodes = graph.data.nodes.sort((a, b) => ((a.id > b.id) ? 1 : -1));
+      const visEdges = graph.data.edges;
+
+      const expectedLabels = [
+        'TASK\nDummy task',
+        'ISSUE\nHow should we brew coffee?',
+        'DECISION\nUse a french press to brew coffee!',
+        'ALTERNATIVE\nUse a filter coffee machine',
+      ];
+      const expectedGroups = ['task', 'issue', 'decision', 'alternative'];
+
+      chai.expect(visNodes).to.have.length(4);
+      chai.expect(visEdges).to.have.length(3);
+
+      // TODO test more things about the edges than the length
+      visNodes.forEach((node, index) => {
+        chai.expect(node.label).to.eql(expectedLabels[index]);
+        chai.expect(node.group).to.eql(expectedGroups[index]);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
 });
