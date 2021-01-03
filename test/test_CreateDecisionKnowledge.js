b3327fe375f5dd24aa717e7e935134c02575f3ec
Julia <18719332+aaaailuj@users.noreply.github.com>
2021-01-03 10:18:43 +0100

    CONDEC-854: add rules and exceptions to issue creation tests

--- test/test_CreateDecisionKnowledge.js
+++ test/test_CreateDecisionKnowledge.js
@@ -6,12 +6,35 @@ const JSONConfig = require('../config.json');
 const { setUpJira, createJiraIssue } = require('./helpers.js');
 
 describe('TCS: CONDEC-168', () => {
-  before((done) => {
+  before(async () => {
     // explicitly use issue persistence strategy here
     setUpJira(true);
-    done();
   });
-  it('should create a decision knowledge issue via the create issue interface', async () => {
+  it(
+    '(R1) If the decision knowledge element is created within an existing knowledge element ' +
+      '(Jira issue or code file), a link is created between an existing knowledge' +
+      ' element and the new element (CONDEC-291). '
+  );
+  it(
+    '(R2) If the documentation location of the new decision knowledge element is "Jira issue' +
+      'text", a new comment in an existing Jira issue is created, which contains the new decision' +
+      'knowledge element. '
+  );
+  it('(R3) A new alternative has the status "idea".');
+  it('(R4) A new decision has the status "decided".');
+  it(
+    '(R5) A new issue (=decision problem), i.e. an issue without linked decision has' +
+      'the status "unresolved".'
+  );
+  it(
+    '(R6) A Jira issue (i.e. a decision knowledge element documented as an entire Jira issue) can' +
+      'only be created in a view on the knowledge graph if the user has the rights to create Jira' +
+      'issues (CONDEC-852, integrity).'
+  );
+  it('(R7) If the webhook is activated, it will be fired (CONDEC-185).');
+  it('(E1) A decision knowledge element with the same id already exists.');
+  it('(E2) The user does not have the rights for creation.');
+  xit('should create a decision knowledge issue via the create issue interface', async () => {
     const options = new firefox.Options();
     options.setProfile(JSONConfig.firefoxProfilePath);
     const driver = await new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
