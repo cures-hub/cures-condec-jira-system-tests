4c28de3435feada1d4efc784e850329e0e97fc43
Julia <18719332+aaaailuj@users.noreply.github.com>
2021-01-07 14:30:45 +0100

    Fix test for (R1)

--- test/test_ChangeDecisionKnowledge.js
+++ test/test_ChangeDecisionKnowledge.js
@@ -3,68 +3,76 @@ const chai = require('chai');
 
 const JSONConfig = require('../config.json');
 const {
-  jira, setUpJira, createJiraIssue, localCredentialsObject,
+  jira,
+  setUpJira,
+  createJiraIssue,
+  localCredentialsObject,
+  createDecisionKnowledgeElement,
+  getKnowledgeElements,
+  updateDecisionKnowledgeElement,
 } = require('./helpers.js');
+
+chai.use(require('chai-like'));
+chai.use(require('chai-things'));
 
 describe('TCS: CONDEC-169', () => {
   before(async () => {
     await setUpJira(true); // These tests need the Jira issue strategy, so we explicitly set it here
   });
-  it('(R1) should change the status when the knowledge type is changed', async () => {
+  it('(R1) If an alternative is changed to a decision, its status is changed to "decided".', async () => {
     // Create a Jira issue and a comment containing decision knowledge
-    const createdIssue = await createJiraIssue('Task', 'Enable persistence of user data');
-    await jira.addComment(
-      createdIssue.id,
-      `{issue}Which database should be used to store user data?{issue}
-      {alternative}Use Postgres to store user data!{alternative}
-      {alternative}Use SQLite to store user data!{alternative}`,
+    const jiraTask = await createJiraIssue(
+      'Task',
+      'Enable persistence of user data'
     );
 
-    // get the id of the sqlite element so we can change its type from alternative to decision
-    const sqliteAlternativeElement = await axios
-      .post(`${JSONConfig.fullUrl}/rest/condec/latest/knowledge/knowledgeElements.json`,
-        { projectKey: JSONConfig.projectKey, searchTerm: 'SQLite' },
-        localCredentialsObject);
-    const idOfSqliteElement = sqliteAlternativeElement.data[0].id;
-    await axios.post(
-      `${JSONConfig.fullUrl}/rest/condec/latest/knowledge/updateDecisionKnowledgeElement.json`,
-      {
-        id: idOfSqliteElement,
-        summary: null,
-        type: 'Decision', // here we change from "Alternative" to "Decision"
-        projectKey: JSONConfig.projectKey,
-        description: null,
-        documentationLocation: 's',
-        status: null,
-      },
-      localCredentialsObject,
+    const issue = await createDecisionKnowledgeElement(
+      'Which database should be used to store user data?',
+      'Issue',
+      's',
+      jiraTask.id,
+      'i'
     );
-
-    // Check that the status changed
-    const sqliteDecisionElement = await axios
-      .post(`${JSONConfig.fullUrl}/rest/condec/latest/knowledge/knowledgeElements.json`,
-        { projectKey: JSONConfig.projectKey, searchTerm: 'SQLite' },
-        localCredentialsObject);
-    chai.expect(sqliteDecisionElement.data[0].status).to.eql('decided');
-    chai.expect(sqliteDecisionElement.data[0].type).to.eql('Decision');
+    const alternative = await createDecisionKnowledgeElement(
+      'Use Postgres to store user data!',
+      'Alternative',
+      's',
+      jiraTask.id,
+      'i'
+    );
+    const updatedAlternative = Object.assign(alternative, { type: 'Decision' });
+    await updateDecisionKnowledgeElement(issue.id, 'i', updatedAlternative);
+    const decisionKnowledgeAfterChange = await getKnowledgeElements();
+    chai
+      .expect(decisionKnowledgeAfterChange)
+      .to.be.an('Array')
+      .that.contains.something.like({
+        id: alternative.id,
+        type: 'Decision',
+        status: 'decided',
+      });
   });
 
   // This test currently fails, since the system sets the knowledge type
   // to "Other" and not "Alternative"
   xit('(R2) should change the knowledge type when the status is changed', async () => {
-    const createdIssue = await createJiraIssue('Task', 'Add a toggle for enabling data persistence');
+    const createdIssue = await createJiraIssue(
+      'Task',
+      'Add a toggle for enabling data persistence'
+    );
     await jira.addComment(
       createdIssue.id,
       `{issue}Who should be allowed to use the toggle?{issue}
       {decision}Everyone should be allowed to set the toggle!{decision}
-      {alternative}Only users with admin rights should be allowed to set the toggle!{alternative}`,
+      {alternative}Only users with admin rights should be allowed to set the toggle!{alternative}`
     );
 
     // get the id of the decision element so we can change its status to rejected
-    const decisionElement = await axios
-      .post(`${JSONConfig.fullUrl}/rest/condec/latest/knowledge/knowledgeElements.json`,
-        { projectKey: JSONConfig.projectKey, searchTerm: 'Everyone' },
-        localCredentialsObject);
+    const decisionElement = await axios.post(
+      `${JSONConfig.fullUrl}/rest/condec/latest/knowledge/knowledgeElements.json`,
+      { projectKey: JSONConfig.projectKey, searchTerm: 'Everyone' },
+      localCredentialsObject
+    );
     const idOfDecision = decisionElement.data[0].id;
     await axios.post(
       `${JSONConfig.fullUrl}/rest/condec/latest/knowledge/updateDecisionKnowledgeElement.json`,
@@ -76,14 +84,15 @@ describe('TCS: CONDEC-169', () => {
         documentationLocation: 's',
         status: 'rejected',
       },
-      localCredentialsObject,
+      localCredentialsObject
     );
 
     // Check that the knowledge type also changed
-    const formerDecisionElement = await axios
-      .post(`${JSONConfig.fullUrl}/rest/condec/latest/knowledge/knowledgeElements.json`,
-        { projectKey: JSONConfig.projectKey, searchTerm: 'Everyone' },
-        localCredentialsObject);
+    const formerDecisionElement = await axios.post(
+      `${JSONConfig.fullUrl}/rest/condec/latest/knowledge/knowledgeElements.json`,
+      { projectKey: JSONConfig.projectKey, searchTerm: 'Everyone' },
+      localCredentialsObject
+    );
     chai.expect(formerDecisionElement.data[0].status).to.eql('rejected'); // sanity check
     chai.expect(formerDecisionElement.data[0].type).to.eql('Alternative');
   });
@@ -91,28 +100,34 @@ describe('TCS: CONDEC-169', () => {
   // seems like the system doesn't allow changing the location...
   xit(`(R4) should delete comment and create Jira issue instead when an element's location is
   changed from comment to Jira issue`, async () => {
-    const createdIssue = await createJiraIssue('Task', 'Add color-coding to persistence strategy page');
+    const createdIssue = await createJiraIssue(
+      'Task',
+      'Add color-coding to persistence strategy page'
+    );
     await jira.addComment(
       createdIssue.id,
       `{issue}Which color should represent that data is stored locally?{issue}
-      {decision}Green should be used to represent locally stored data!{decision}`,
+      {decision}Green should be used to represent locally stored data!{decision}`
     );
 
     // get the id of the decision element so we can change its status to rejected
-    const decisionElement = await axios
-      .post(`${JSONConfig.fullUrl}/rest/condec/latest/knowledge/knowledgeElements.json`, {
+    const decisionElement = await axios.post(
+      `${JSONConfig.fullUrl}/rest/condec/latest/knowledge/knowledgeElements.json`,
+      {
         projectKey: JSONConfig.projectKey,
         searchTerm: 'Which color',
       },
-      localCredentialsObject);
+      localCredentialsObject
+    );
     const idOfDecision = decisionElement.data[0].id;
     await axios.post(
-      `${JSONConfig.fullUrl}/rest/condec/latest/knowledge/updateDecisionKnowledgeElement.json`, {
+      `${JSONConfig.fullUrl}/rest/condec/latest/knowledge/updateDecisionKnowledgeElement.json`,
+      {
         id: idOfDecision,
         projectKey: JSONConfig.projectKey,
         documentationLocation: 'i', // change to issue here
       },
-      localCredentialsObject,
+      localCredentialsObject
     );
 
     // Check that the knowledge type also changed
@@ -125,6 +140,10 @@ describe('TCS: CONDEC-169', () => {
     // chai.expect(formerDecisionElement.data[0].status).to.eql('rejected'); // sanity check
     // chai.expect(formerDecisionElement.data[0].type).to.eql('Alternative');
   });
-  it('(R5) should update the comment containing an element that is changed via the changeElement interface');
-  it('(E) should throw an error when the element with given id and documentation location does not exist in database');
+  it(
+    '(R5) should update the comment containing an element that is changed via the changeElement interface'
+  );
+  it(
+    '(E) should throw an error when the element with given id and documentation location does not exist in database'
+  );
 });
