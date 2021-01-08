6d511c7c4e6e0fdb0b03725715c56dfedd27f41f
Julia <18719332+aaaailuj@users.noreply.github.com>
2021-01-08 13:18:47 +0100

    Break test case for R1 into two separate cases

--- test/test_DecisionKnowledgeInComments.js
+++ test/test_DecisionKnowledgeInComments.js
@@ -15,7 +15,7 @@ describe('TCS: CONDEC-123', () => {
     await setUpJira();
   });
   it(
-    '(R1) Both macro tags and icons/emojis can be used to manually classify/annotate' +
+    '(R1) (part 1) Macro tags can be used to manually classify/annotate' +
       ' text as an decision knowledge element',
     /*
       - {issue} How to implement ...? {issue}; icon: (!)
@@ -25,39 +25,102 @@ describe('TCS: CONDEC-123', () => {
       - {con} Reduces usability. {con}; icon: (-) 
      */
     async () => {
-      const issue = await createJiraIssue(
+      // Case one: using macro tags
+      const issue1 = await createJiraIssue(
         'Task',
         'Create a blog page for the Moo company'
       );
       await jira.addComment(
-        issue.id,
+        issue1.id,
         '{issue}Which site generator should be used to make the blog?{issue}\n' +
           '{decision}Use Jekyll to make the blog!{decision}\n' +
           '{pro}Jekyll is free{pro}\n' +
           '{con}Jekyll has a learning curve{con}\n' +
-          '{alternative}Use WordPress to make the blog!{alternative}'
+          '{alternative}Use WordPress to make the blog!{alternative}\n'
       );
-      const knowledgeElements = await getKnowledgeElements('', issue.key);
-      chai.expect(knowledgeElements).to.have.lengthOf(6);
-      chai.expect(knowledgeElements).to.contain.something.like({
-        type: 'Issue',
-        summary: 'Which site generator should be used to make the blog?',
+      const knowledgeElements1 = await getKnowledgeElements('', issue1.key);
+
+      const expectedPartialElements = [
+        {
+          type: 'Issue',
+          summary: 'Which site generator should be used to make the blog?',
+        },
+        {
+          type: 'Decision',
+          summary: 'Use Jekyll to make the blog!',
+        },
+        {
+          type: 'Pro',
+          summary: 'Jekyll is free',
+        },
+        {
+          type: 'Con',
+          summary: 'Jekyll has a learning curve',
+        },
+        {
+          type: 'Alternative',
+          summary: 'Use WordPress to make the blog!',
+        },
+      ];
+
+      expectedPartialElements.forEach((partialElement) => {
+        chai
+          .expect(knowledgeElements1)
+          .to.contain.something.like(partialElement);
       });
-      chai.expect(knowledgeElements).to.contain.something.like({
-        type: 'Decision',
-        summary: 'Use Jekyll to make the blog!',
-      });
-      chai.expect(knowledgeElements).to.contain.something.like({
-        type: 'Pro',
-        summary: 'Jekyll is free',
-      });
-      chai.expect(knowledgeElements).to.contain.something.like({
-        type: 'Con',
-        summary: 'Jekyll has a learning curve',
-      });
-      chai.expect(knowledgeElements).to.contain.something.like({
-        type: 'Alternative',
-        summary: 'Use WordPress to make the blog!',
+    }
+  );
+  it(
+    '(R1) (part 2) Icons/emojis can be used to manually classify/annotate' +
+      ' text as an decision knowledge element',
+    async () => {
+      // Case two
+      // Jira project exists and ConDec plugin is activated/enabled for the
+      // project, Jira issue exists
+      const issue2 = await createJiraIssue(
+        'Task',
+        'Decide on the domain name for the blog'
+      );
+      // Step 1: add a comment containing manually classified decision knowledge
+      // with Jira emoji
+      await jira.addComment(
+        issue2.id,
+        '(!) Which domain registrar should be used?\n' +
+          '(/) Use openSRS for registering the domain name!\n' +
+          '(+) OpenSRS has low fees\n' +
+          '(on) Use GoDaddy for registering the domain name!\n' +
+          '(-) GoDaddy has high fees'
+      );
+      // Step 2: examine the knowledge elements that are in the database. Make
+      // sure all of the added elements exist
+      const knowledgeElements2 = await getKnowledgeElements('', issue2.key);
+      const expectedPartialElements = [
+        {
+          type: 'Issue',
+          summary: 'Which domain registrar should be used?',
+        },
+        {
+          type: 'Decision',
+          summary: 'Use openSRS for registering the domain name!',
+        },
+        {
+          type: 'Pro',
+          summary: 'OpenSRS has low fees',
+        },
+        {
+          type: 'Con',
+          summary: 'GoDaddy has high fees',
+        },
+        {
+          type: 'Alternative',
+          summary: 'Use GoDaddy for registering the domain name!',
+        },
+      ];
+
+      expectedPartialElements.forEach((partialElement) => {
+        chai
+          .expect(knowledgeElements2)
+          .to.contain.something.like(partialElement);
       });
     }
   );
