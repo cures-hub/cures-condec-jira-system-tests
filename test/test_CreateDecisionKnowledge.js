e43879b58abe1e23548c6081a9b159c1f1ac12f3
Julia <18719332+aaaailuj@users.noreply.github.com>
2021-01-10 11:49:49 +0100

    Rename test cases for better readability

--- test/test_CreateDecisionKnowledge.js
+++ test/test_CreateDecisionKnowledge.js
@@ -16,31 +16,25 @@ describe('TCS: Test create decision knowledge', () => {
     // explicitly use issue persistence strategy here
     await setUpJira(true);
   });
+  it('should create a new comment for an existing Jira issue when the documentation location "Jira issue text" is selected (R2)', async () => {
+    const task = await createJiraIssue('Task', 'Dummy task for R2');
+    await createDecisionKnowledgeElement(
+      'Dummy decision knowledge issue for R2',
+      'Issue',
+      's',
+      task.id,
+      'i'
+    );
+    const taskAfterUpdate = await jira.findIssue(task.key);
 
-  it(
-    '(R2) If the documentation location of the new decision knowledge element is "Jira issue ' +
-      'text", a new comment in an existing Jira issue is created, which contains the new decision' +
-      ' knowledge element.',
-    async () => {
-      const task = await createJiraIssue('Task', 'Dummy task for R2');
-      await createDecisionKnowledgeElement(
-        'Dummy decision knowledge issue for R2',
-        'Issue',
-        's',
-        task.id,
-        'i'
-      );
-      const taskAfterUpdate = await jira.findIssue(task.key);
-
-      chai
-        .expect(taskAfterUpdate.fields.comment.comments)
-        .to.be.an('Array')
-        .that.contains.something.like({
-          body: '{issue}Dummy decision knowledge issue for R2\n{issue}',
-        });
-    }
-  );
-  it('(R3) A new alternative has the status "idea".', async () => {
+    chai
+      .expect(taskAfterUpdate.fields.comment.comments)
+      .to.be.an('Array')
+      .that.contains.something.like({
+        body: '{issue}Dummy decision knowledge issue for R2\n{issue}',
+      });
+  });
+  it('should give a new alternative the status "idea" (R3) ', async () => {
     // Precondition: Jira issue exists
     const issue = await createJiraIssue(
       'Issue',
@@ -62,7 +56,7 @@ describe('TCS: Test create decision knowledge', () => {
     chai.expect(alternative).to.have.property('type', 'Alternative');
   });
 
-  it('(R4) A new decision has the status "decided".', async () => {
+  it('should give a new decision the status "decided" (R4)', async () => {
     const issue = await createJiraIssue('Issue', 'Dummy issue for R4');
     const decision = await createDecisionKnowledgeElement(
       'dummy decision for R4',
@@ -75,19 +69,15 @@ describe('TCS: Test create decision knowledge', () => {
     chai.expect(decision).to.have.property('status', 'decided');
     chai.expect(decision).to.have.property('type', 'Decision');
   });
-  it(
-    '(R5) A new issue (=decision problem), i.e. an issue without linked decision has' +
-      ' the status "unresolved".',
-    async () => {
-      const issue = await createDecisionKnowledgeElement(
-        'Dummy issue for R5',
-        'Issue',
-        'i'
-      ); // by default this issue is unlinked
+  it('should give a new decision knowledge issue without a linked decision the status "unresolved" (R5)', async () => {
+    const issue = await createDecisionKnowledgeElement(
+      'Dummy issue for R5',
+      'Issue',
+      'i'
+    ); // by default this issue is unlinked
 
-      chai.expect(issue).to.have.property('summary', 'Dummy issue for R5');
-      chai.expect(issue).to.have.property('status', 'unresolved');
-      chai.expect(issue).to.have.property('type', 'Issue');
-    }
-  );
+    chai.expect(issue).to.have.property('summary', 'Dummy issue for R5');
+    chai.expect(issue).to.have.property('status', 'unresolved');
+    chai.expect(issue).to.have.property('type', 'Issue');
+  });
 });
