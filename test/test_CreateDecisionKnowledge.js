5a0e0a5721a94a3e8ea13a677b22ff7bb718b305
Julia <18719332+aaaailuj@users.noreply.github.com>
2021-01-10 11:09:20 +0000

    CONDEC-854: Readability improvements for issue creation tests (#19)
    * Refactor test for (R2) to use helper function
    
    * Remove unused imports
    
    * Refactor (R3) to use createDecisionKnowledge helper
    
    * Refactor (R3) further to be more concise
    
    * Refactor test for (R4)
    
    * Refactor test case for (R4)
    
    * fix typo
    
    * Remove console statements as they spam the test output
    
    * Refactor test case for (R3)
    
    * Remove test cases that will not be implemented
    
    * Rename describe block to conform to decision about readability
    
    * Rename test cases for better readability
    
    * Add specifications for each test case

--- test/test_CreateDecisionKnowledge.js
+++ test/test_CreateDecisionKnowledge.js
@@ -10,60 +10,101 @@ const {
 chai.use(require('chai-like'));
 chai.use(require('chai-things'));
 
-describe('TCS: CONDEC-168', () => {
+describe('TCS: Test create decision knowledge', () => {
   // reset Jira project for every test case, to ensure no interference
   beforeEach(async () => {
     // explicitly use issue persistence strategy here
     await setUpJira(true);
   });
-  xit(
-    // This is out of scope of test plan v1
-    '(R1) If the decision knowledge element is created within an existing knowledge element ' +
-      '(Jira issue or code file), a link is created between an existing knowledge' +
-      ' element and the new element (CONDEC-291). '
-  );
-  it(
-    '(R2) If the documentation location of the new decision knowledge element is "Jira issue ' +
-      'text", a new comment in an existing Jira issue is created, which contains the new decision' +
-      ' knowledge element.',
-    async () => {
-      const task = await createJiraIssue('Task', 'Dummy task for R2');
-      await createDecisionKnowledgeElement(
-        'Dummy decision knowledge issue for R2',
-        'Issue',
-        's',
-        task.id,
-        'i'
-      );
-      const taskAfterUpdate = await jira.findIssue(task.key);
 
-      chai
-        .expect(taskAfterUpdate.fields.comment.comments)
-        .to.be.an('Array')
-        .that.contains.something.like({
-          body: '{issue}Dummy decision knowledge issue for R2\n{issue}',
-        });
-    }
-  );
-  it('(R3) A new alternative has the status "idea".', async () => {
-    const issue = await createJiraIssue('Issue', 'Dummy issue for R3');
+  /**
+   * TCS: Test create decision knowledge should create a new comment for an existing Jira issue when the documentation location "Jira issue text" is selected (R2)
+   *
+   * Precondition: Jira issue exists
+   *
+   * Step 1: Create a decision knowledge element with the documentation location
+   *    'Jira issue text' (= docuementation location 's' in the backend)
+   *
+   * Step 2: Verify that the Jira issue has a comment containing the added
+   *     decision knowledge element
+   *
+   * Postcondition: The Jira issue has a comment containing the added decision
+   *     knowledge element
+   */
+  it('should create a new comment for an existing Jira issue when the documentation location "Jira issue text" is selected (R2)', async () => {
+    // Precondition: Jira issue exists
+    const task = await createJiraIssue('Task', 'Dummy task for R2');
+
+    // Step 1: Create a decision knowledge element with the documentation
+    // location 'Jira issue text' (=docuementation location 's' in the backend)
+    await createDecisionKnowledgeElement(
+      'Dummy decision knowledge issue for R2',
+      'Issue',
+      's',
+      task.id,
+      'i'
+    );
+
+    // Step 2: Verify that the Jira issue has a comment containing the added decision knowledge element
+    const taskAfterUpdate = await jira.findIssue(task.key);
+
+    chai
+      .expect(taskAfterUpdate.fields.comment.comments)
+      .to.be.an('Array')
+      .that.contains.something.like({
+        body: '{issue}Dummy decision knowledge issue for R2\n{issue}',
+      });
+  });
+
+  /**
+   * TCS: Test create decision knowledge should give a new alternative the status "idea" (R3)
+   *
+   * Precondition: Jira issue exists
+   *
+   * Step 1: Create a new alternative
+   *
+   * Step 2: Verify that the new alternative has the status 'idea'
+   *
+   * Postcondition: The new alternative has the status idea
+   */
+  it('should give a new alternative the status "idea" (R3)', async () => {
+    // Precondition: Jira issue exists
+    const issue = await createJiraIssue(
+      'Issue',
+      'Which platforms should the app support?'
+    );
+    // Step 1: Create a new alternative
     const alternative = await createDecisionKnowledgeElement(
-      'dummy alternative for R3',
+      'The app should support Android!',
       'Alternative',
       's',
       issue.id,
       'i'
     );
-
+    // Step 2: Verify that the new alternative has the status 'idea'
     chai
       .expect(alternative)
-      .to.have.property('summary', 'dummy alternative for R3');
+      .to.have.property('summary', 'The app should support Android!');
     chai.expect(alternative).to.have.property('status', 'idea');
     chai.expect(alternative).to.have.property('type', 'Alternative');
   });
 
-  it('(R4) A new decision has the status "decided".', async () => {
+  /**
+   * TCS: Test create decision knowledge should give a new decision the status "decided" (R4)
+   *
+   * Precondition: Jira issue exists
+   *
+   * Step 1: Create a decision element
+   *
+   * Step 2: Verify that the decision has the status "decided"
+   *
+   * Postcondition: The decision has the status "decided"
+   */
+  it('should give a new decision the status "decided" (R4)', async () => {
+    // Precondition: Jira issue exists
     const issue = await createJiraIssue('Issue', 'Dummy issue for R4');
+
+    // Step 1: Create a decision element
     const decision = await createDecisionKnowledgeElement(
       'dummy decision for R4',
       'Decision',
@@ -71,41 +112,33 @@ describe('TCS: CONDEC-168', () => {
       issue.id,
       'i'
     );
+
+    // Step 2: Verify that the decision has the status "decided"
     chai.expect(decision).to.have.property('summary', 'dummy decision for R4');
     chai.expect(decision).to.have.property('status', 'decided');
     chai.expect(decision).to.have.property('type', 'Decision');
   });
-  it(
-    '(R5) A new issue (=decision problem), i.e. an issue without linked decision has' +
-      ' the status "unresolved".',
-    async () => {
-      const issue = await createDecisionKnowledgeElement(
-        'Dummy issue for R5',
-        'Issue',
-        'i'
-      ); // by default this issue is unlinked
 
-      chai.expect(issue).to.have.property('summary', 'Dummy issue for R5');
-      chai.expect(issue).to.have.property('status', 'unresolved');
-      chai.expect(issue).to.have.property('type', 'Issue');
-    }
-  );
+  /**
+   * TCS: Test create decision knowledge should give a new decision knowledge issue without a linked decision the status "unresolved" (R5)
+   *
+   * Precondition: None
+   *
+   * Step 1: Create a new decision knowledge issue
+   *
+   * Step 2: Verify the status of the new issue is "unresolved"
+   *
+   * Postcondition: The status of the new issue is "unresolved"
+   */
+  it('should give a new decision knowledge issue without a linked decision the status "unresolved" (R5)', async () => {
+    const issue = await createDecisionKnowledgeElement(
+      'Dummy issue for R5',
+      'Issue',
+      'i'
+    ); // by default this issue is unlinked
 
-  xit(
-    // This will be tested elsewhere
-    '(R6) A Jira issue (i.e. a decision knowledge element documented as an entire Jira issue) can' +
-      ' only be created in a view on the knowledge graph if the user has the rights to create Jira' +
-      ' issues (CONDEC-852, integrity).'
-  );
-
-  // This will be tested elsewhere
-  xit('(R7) If the webhook is activated, it will be fired (CONDEC-185).');
-
-  // Currently there is no way to trigger this through the UI, so we don't test it here.
-  xit(
-    '(E1) A decision knowledge element with the same summary and description already exists.'
-  );
-
-  // This will be tested elsewhere
-  xit('(E2) The user does not have the rights for creation.');
+    chai.expect(issue).to.have.property('summary', 'Dummy issue for R5');
+    chai.expect(issue).to.have.property('status', 'unresolved');
+    chai.expect(issue).to.have.property('type', 'Issue');
+  });
 });
