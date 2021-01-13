5fa532a28318ba134973d39f97be80d287445d9f
Julia <18719332+aaaailuj@users.noreply.github.com>
2021-01-13 21:15:08 +0100

    Add specifications for linking and unlinking tests

--- test/test_linkAndUnlinkDecisionKnowledge.js
+++ test/test_linkAndUnlinkDecisionKnowledge.js
@@ -21,6 +21,18 @@ describe('TCS: Test link knowledge elements', () => {
   before(async () => {
     await setUpJira(true); // turn issue strategy on
   });
+
+  /**
+   * TCS: Test link knowledge elements should create a Jira issue link when both the source and destination elements are Jira issues (R1)
+   *
+   * Precondition: Two decision knowledge elements exist as Jira issues
+   *
+   * Step 1: Create a ConDec link of type "relates" between the two elements
+   *
+   * Step 2: Verify that a new link was created with type "relates"
+   *
+   * Postcondition: A new link with the given type was created
+   */
   it('should create a Jira issue link when both the source and destination elements are Jira issues (R1)', async () => {
     const issue1 = await createJiraIssue('Issue', 'Issue 1');
     const issue2 = await createJiraIssue('Alternative', 'Issue 2');
@@ -67,6 +79,20 @@ describe('TCS: Test link knowledge elements', () => {
       .to.have.property('key', `${issue1.key}`);
   });
 
+  /**
+   * TCS: Test link knowledge elements should store the link between elements with documentation location "i" and documentation location "s" in the ConDec database, not a Jira issue link (R2)
+   *
+   * Precondition: Two decision knowledge elements exist, one with documentation
+   * location "i" and the other with documentation location "s"
+   *
+   * Step 1: Create a ConDec link between the two elements
+   *
+   * Step 2: Verify that no Jira link was created and that the ConDec link is
+   * stored in the ConDec database
+   *
+   * Postcondition: No Jira link was created and that the ConDec link is
+   * stored in the ConDec database
+   */
   it('should store the link between elements with documentation location "i" and documentation location "s" in the ConDec database, not a Jira issue link (R2)', async () => {
     const decisionKnowledgeIssue = await createDecisionKnowledgeElement(
       'Which method of transportation should be used for pizza delivery?',
@@ -91,9 +117,21 @@ describe('TCS: Test link knowledge elements', () => {
       .to.contain(decisionKnowledgeIssue.key);
   });
 
+  /**
+   * TCS: Test link knowledge elements should not allow an element with documentation location "i" to be linked to itself (R3)
+   *
+   * Precondition: A decision knowledge element exists as a Jira issue
+   *
+   * Step 1: Attempt to create a ConDec link between the decision knowledge
+   * element and itself
+   *
+   * Step 2: Verify that no link was created and nothing changed
+   *
+   * Postcondition: Nothing changed; no link was created
+   */
   // This is currently failing, as it should not be allowed to link an element
   // to itself
-  it('should not allow an element to be linked to itself (R3)', async () => {
+  it('should not allow an element with documentation location "i" to be linked to itself (R3)', async () => {
     const alternative = await createJiraIssue(
       'Alternative',
       'Dummy Alternative'
@@ -113,6 +151,18 @@ describe('TCS: Test link knowledge elements', () => {
     );
     chai.expect(link.status).not.to.eql(200);
   });
+
+  /**
+   * TCS: Test link knowledge elements should set the status of an issue to "resolved" when it is linked to a decision with status "decided" (R4)
+   *
+   * Precondition: Decision knowledge issue exists
+   *
+   * Step 1: Create a decision as a child of the issue element
+   *
+   * Step 2: Verify that issue has status resolved
+   *
+   * Postcondition: The decision knowledge issue has status "resolved"
+   */
   it('should set the status of an issue to "resolved" when it is linked to a decision with status "decided" (R4)', async () => {
     const issue1 = await createJiraIssue(
       'Issue',
@@ -125,7 +175,7 @@ describe('TCS: Test link knowledge elements', () => {
       issue1.id,
       'i'
     );
-    chai.expect(decisionElement).to.have.property('status', 'decided');
+    chai.expect(decisionElement).to.have.property('status', 'decided'); // sanity check
     const knowledgeElements = await getKnowledgeElements();
 
     chai
@@ -138,6 +188,18 @@ describe('TCS: Test link knowledge elements', () => {
       });
   });
 
+  /**
+   * TCS: Test link knowledge elements should not allow an element that does not exist in the database to be linked (E1)
+   *
+   * Precondition: A decision knowledge element exists
+   *
+   * Step 1: Trigger link creation between the existing element and element with
+   * the id -1
+   *
+   * Step 2: Verify that this does not work
+   *
+   * Postcondition: Nothing changed
+   */
   it('should not allow an element that does not exist in the database to be linked (E1)', async () => {
     const issue = await createDecisionKnowledgeElement(
       'Dummy issue',
@@ -162,6 +224,7 @@ describe('TCS: Test link knowledge elements', () => {
   });
 });
 
+// TODO: move this to a separate file
 /**
  * CONDEC-172: Unlink knowledge elements
  */
@@ -169,6 +232,20 @@ describe('TCS: Test unlink knowledge elements', () => {
   before(async () => {
     await setUpJira(true); // turn issue strategy on
   });
+
+  /**
+   * TCS: Test unlink knowledge elements should delete the Jira issue link when both source and target elements of unlinking are Jira issues (R1)
+   *
+   * Precondition: Two decision knowledge elements exist as Jira issues and are
+   * linked to each other
+   *
+   * Step 1: Request the link between the issues to be deleted via the ConDec
+   * deleteLink interface
+   *
+   * Step 2: Verify that the Jira issue link between the elements has been removed
+   *
+   * Postcondition: The Jira issue link between the elements has been removed
+   */
   it('should delete the Jira issue link when both source and target elements of unlinking are Jira issues (R1)', async () => {
     const issue1 = await createJiraIssue('Issue', 'Issue 1');
     const issue2 = await createJiraIssue('Alternative', 'Issue 2');
@@ -221,6 +298,18 @@ describe('TCS: Test unlink knowledge elements', () => {
       .with.length(0);
   });
 
+  /**
+   * TCS: Test unlink knowledge elements should set the status of an issue to "unresolved" when it is unlinked from a "decided" decision and has no other "decided" decisions linked (R3)
+   *
+   * Precondition: A decision knowledge issue exists and is linked to a decision
+   * in status "decided"
+   *
+   * Step 1: Delete the link between the knowledge issue and the decision
+   *
+   * Step 2: Verify that the issue now has status "unresolved"
+   *
+   * Postcondition: The issue's status is "unresolved"
+   */
   // This test is currently failing, it seems as if the issue gets deleted and
   // not just unlinked
   it('should set the status of an issue to "unresolved" when it is unlinked from a "decided" decision and has no other "decided" decisions linked (R3)', async () => {
@@ -263,7 +352,19 @@ describe('TCS: Test unlink knowledge elements', () => {
       .that.contains.something.like({ id: issue.id, status: 'unresolved' });
   });
 
-  it('should not allow unlinking of elements with nonexistent ids (E1)', async () => {
+  /**
+   * TCS: Test unlink knowledge elements should not allow unlinking of elements with nonexistent ids (E1)
+   *
+   * Precondition: Two decision knowledge elements exist and are not linked to
+   * each other
+   *
+   * Step 1: Attempt to trigger deletion of the link between the two elements
+   *
+   * Step 2: Verify that the deletion failed
+   *
+   * Postcondition: Nothing changed
+   */
+  it.only("should not allow unlinking of a link that doesn't exist (E1)", async () => {
     const issue = await createDecisionKnowledgeElement(
       'How should files be organized?',
       'Issue',
@@ -297,7 +398,7 @@ describe('TCS: Test unlink knowledge elements', () => {
     try {
       await axios(deleteLinkRequest);
     } catch (err) {
-      chai.expect(err.message).to.eql('Request failed with status code 500');
+      chai.assert(err);
     }
   });
 });
