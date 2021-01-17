95f8be6d40d46481fce3b1514236131b9aa0e4f9
Julia <18719332+aaaailuj@users.noreply.github.com>
2021-01-17 18:22:20 +0100

    Update specifications for linking tests

--- test/test_LinkKnowledgeElements.js
+++ test/test_LinkKnowledgeElements.js
@@ -24,13 +24,14 @@ describe('TCS: Test link knowledge elements', () => {
   /**
    * TCS: Test link knowledge elements should create a Jira issue link when both the source and destination elements are Jira issues (R1)
    *
-   * Precondition: Two decision knowledge elements exist as Jira issues
-   *
-   * Step 1: Create a ConDec link of type "relates" between the two elements
-   *
-   * Step 2: Verify that a new link was created with type "relates"
-   *
-   * Postcondition: A new link with the given type was created
+   * System function: Link knowledge elements
+   * Precondition system: Two decision knowledge elements exist as Jira issues
+   * Precondition GUI: WS1.3 or WS1.4
+   * Test steps:
+      1. Create a link of type "relates" between the two elements via the ConDec interface
+   * Expected result on GUI: The new link is visible on the knowledge graph. A success message is displayed.
+   * Expected exception: None
+   * Postcondition system: A new Jira issue link was created with type "relates"
    */
   it('should create a Jira issue link when both the source and destination elements are Jira issues (R1)', async () => {
     const issue1 = await createJiraIssue('Issue', 'Issue 1');
@@ -51,46 +52,28 @@ describe('TCS: Test link knowledge elements', () => {
     const issue1Links = await jira.findIssue(issue1.id);
     const issue2Links = await jira.findIssue(issue2.id);
 
-    chai
-      .expect(issue1Links.fields.issuelinks[0])
-      .to.have.deep.property('id', `${link.data.id}`);
-    chai
-      .expect(issue1Links.fields.issuelinks[0].type)
-      .to.have.property('name', 'Relates');
-    chai
-      .expect(issue1Links.fields.issuelinks[0].outwardIssue)
-      .to.have.property('id', `${issue2.id}`);
-    chai
-      .expect(issue1Links.fields.issuelinks[0].outwardIssue)
-      .to.have.property('key', `${issue2.key}`);
+    chai.expect(issue1Links.fields.issuelinks[0]).to.have.deep.property('id', `${link.data.id}`);
+    chai.expect(issue1Links.fields.issuelinks[0].type).to.have.property('name', 'Relates');
+    chai.expect(issue1Links.fields.issuelinks[0].outwardIssue).to.have.property('id', `${issue2.id}`);
+    chai.expect(issue1Links.fields.issuelinks[0].outwardIssue).to.have.property('key', `${issue2.key}`);
 
-    chai
-      .expect(issue2Links.fields.issuelinks[0])
-      .to.have.deep.property('id', `${link.data.id}`);
-    chai
-      .expect(issue2Links.fields.issuelinks[0].type)
-      .to.have.property('name', 'Relates');
-    chai
-      .expect(issue2Links.fields.issuelinks[0].inwardIssue)
-      .to.have.property('id', `${issue1.id}`);
-    chai
-      .expect(issue2Links.fields.issuelinks[0].inwardIssue)
-      .to.have.property('key', `${issue1.key}`);
+    chai.expect(issue2Links.fields.issuelinks[0]).to.have.deep.property('id', `${link.data.id}`);
+    chai.expect(issue2Links.fields.issuelinks[0].type).to.have.property('name', 'Relates');
+    chai.expect(issue2Links.fields.issuelinks[0].inwardIssue).to.have.property('id', `${issue1.id}`);
+    chai.expect(issue2Links.fields.issuelinks[0].inwardIssue).to.have.property('key', `${issue1.key}`);
   });
 
   /**
    * TCS: Test link knowledge elements should store the link between elements with documentation location "i" and documentation location "s" in the ConDec database, not a Jira issue link (R2)
    *
-   * Precondition: Two decision knowledge elements exist, one with documentation
-   * location "i" and the other with documentation location "s"
-   *
-   * Step 1: Create a ConDec link between the two elements
-   *
-   * Step 2: Verify that no Jira link was created and that the ConDec link is
-   * stored in the ConDec database
-   *
-   * Postcondition: No Jira link was created and that the ConDec link is
-   * stored in the ConDec database
+   * System function: Link knowledge elements
+   * Precondition system: Two decision knowledge elements exist, one with documentation location Jira issue (="i") and the other with documentation location Jira issue text (="s")
+   * Precondition GUI: WS1.3 or WS1.4
+   * Test steps:
+     1. Create a link between the two decision knowledge elements via the ConDec interface
+   * Expected result on GUI: The link is shown on the knowledge graph. A success message is shown.
+   * Expected exception: None
+   * Postcondition system: The knowledge element stored in the Jira issue does not have any new Jira issue links. The created link is stored in the ConDec database.
    */
   it('should store the link between elements with documentation location "i" and documentation location "s" in the ConDec database, not a Jira issue link (R2)', async () => {
     const decisionKnowledgeIssue = await createDecisionKnowledgeElement(
@@ -108,33 +91,24 @@ describe('TCS: Test link knowledge elements', () => {
     const jiraIssue = await jira.findIssue(decisionKnowledgeIssue.id);
     // Check that no Jira issue link exists
     chai.expect(jiraIssue.fields.issuelinks).to.be.an('Array').with.lengthOf(0);
-
-    // Check that the comment's key contains the issue key - this means they
-    // are linked in the ConDec database
-    chai
-      .expect(decisionKnowledgeComment.key)
-      .to.contain(decisionKnowledgeIssue.key);
+    chai.expect(decisionKnowledgeComment.key).to.contain(decisionKnowledgeIssue.key);
   });
 
   /**
    * TCS: Test link knowledge elements should not allow an element with documentation location "i" to be linked to itself (R3)
    *
-   * Precondition: A decision knowledge element exists as a Jira issue
-   *
-   * Step 1: Attempt to create a ConDec link between the decision knowledge
-   * element and itself
-   *
-   * Step 2: Verify that no link was created and nothing changed
-   *
-   * Postcondition: Nothing changed; no link was created
+   * System function: Link knowledge elements
+   * Precondition system: A decision knowledge element exists as a Jira issue
+   * Precondition GUI: WS1.3 or WS1.4
+   * Test steps:
+   *   1. Attempt to create a ConDec link between the decision knowledge element and itself
+   * Expected result on GUI: No link is created
+   * Expected exception: An error message appears
+   * Postcondition system: Nothing changed
    */
-  // This is currently failing, as it should not be allowed to link an element
-  // to itself
+  // This is currently failing in v2.2.9, see CONDEC-895
   it('should not allow an element with documentation location "i" to be linked to itself (R3)', async () => {
-    const alternative = await createJiraIssue(
-      'Alternative',
-      'Dummy Alternative'
-    );
+    const alternative = await createJiraIssue('Alternative', 'Dummy Alternative');
 
     // Link the alternative to itself
     const link = await axios.post(
@@ -154,19 +128,17 @@ describe('TCS: Test link knowledge elements', () => {
   /**
    * TCS: Test link knowledge elements should set the status of an issue to "resolved" when it is linked to a decision with status "decided" (R4)
    *
-   * Precondition: Decision knowledge issue exists
-   *
-   * Step 1: Create a decision as a child of the issue element
-   *
-   * Step 2: Verify that issue has status resolved
-   *
-   * Postcondition: The decision knowledge issue has status "resolved"
+   * System function: Link knowledge elements
+   * Precondition system: Decision knowledge issue exists
+   * Precondition GUI: WS1.3 or WS1.4
+   * Test steps:
+       1. Create a decision as a child of the issue element
+   * Expected result on GUI: The decision is linked to the issue element
+   * Expected exception: None
+   * Postcondition system: The decision knowledge issue has status "resolved"
    */
   it('should set the status of an issue to "resolved" when it is linked to a decision with status "decided" (R4)', async () => {
-    const issue1 = await createJiraIssue(
-      'Issue',
-      'Which board games should be played?'
-    );
+    const issue1 = await createJiraIssue('Issue', 'Which board games should be played?');
     const decisionElement = await createDecisionKnowledgeElement(
       'Settlers of Catan should be played!',
       'Decision',
@@ -190,21 +162,18 @@ describe('TCS: Test link knowledge elements', () => {
   /**
    * TCS: Test link knowledge elements should not allow an element that does not exist in the database to be linked (E1)
    *
-   * Precondition: A decision knowledge element exists
+   * System function: Link knowledge elements
+   * Precondition system: A decision knowledge element exists
+   * Precondition GUI: WS1.3 or WS1.4
+   * Test steps:
+   *    1. Trigger link creation between the existing element and element with the id -1 (on the GUI leave the target element empty and click "Link element")
+   * Expected result on GUI: An error message should appear with the message: Link could not be created due to a bad request.
+   * Expected exception: A 400 error occurs
+   * Postcondition system: nothing changed
    *
-   * Step 1: Trigger link creation between the existing element and element with
-   * the id -1
-   *
-   * Step 2: Verify that this does not work
-   *
-   * Postcondition: Nothing changed
    */
   it('should not allow an element that does not exist in the database to be linked (E1)', async () => {
-    const issue = await createDecisionKnowledgeElement(
-      'Dummy issue',
-      'Issue',
-      'i'
-    );
+    const issue = await createDecisionKnowledgeElement('Dummy issue', 'Issue', 'i');
     try {
       await axios.post(
         `${JSONConfig.fullUrl}/rest/condec/latest/knowledge/createLink.json` +
